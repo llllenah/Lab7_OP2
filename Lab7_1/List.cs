677009dbd58a32d2119ad599600eb8a026d5d0ad
Olena Serhiienko <lena066636@gmail.com>
2025-03-14 12:55:09 +0200

    added comments

--- Lab7_1/List.cs
+++ Lab7_1/List.cs
@@ -3,25 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
 /// Simple implementation of a singly linked list for short integers.
+/// </summary>
 public class ShortLinkedList : IEnumerable<short>
 {
+    /// <summary>
+    /// Represents a node in the linked list.
+    /// </summary>
     private class Node
     {
+        /// <summary>
+        /// The value stored in the node.
+        /// </summary>
         public short Value;
+
+        /// <summary>
+        /// Reference to the next node in the list.
+        /// </summary>
         public Node? Next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class with the specified value.
+        /// </summary>
+        /// <param name="value">The value to store in the node.</param>
         public Node(short value) => Value = value;
     }
 
+    /// <summary>
+    /// Reference to the first node of the list.
+    /// </summary>
     private Node? head;
 
+    /// <summary>
     /// Adds an element to the beginning of the list.
+    /// </summary>
+    /// <param name="value">The value to add.</param>
     public void AddToStart(short value)
     {
         head = new Node(value) { Next = head };
     }
 
-    /// Finds the first element that is a multiple of the given number.
+    /// <summary>
+    /// Finds the first element in the list that is a multiple of the given number.
+    /// </summary>
+    /// <param name="multiple">The divisor to check for multiples.</param>
+    /// <returns>The first multiple found, or null if none exist.</returns>
     public short? FindFirstMultipleOf(short multiple)
     {
         foreach (var value in this)
@@ -32,7 +59,10 @@ public class ShortLinkedList : IEnumerable<short>
         return null;
     }
 
-    /// Calculates the product of elements that are less than the average value.
+    /// <summary>
+    /// Calculates the product of all elements that are less than the average value.
+    /// </summary>
+    /// <returns>The product of elements less than the average, or 0 if none exist.</returns>
     public int ProductOfElementsLessThanAverage()
     {
         List<short> values = this.ToList();
@@ -53,7 +83,11 @@ public class ShortLinkedList : IEnumerable<short>
         return found ? product : 0;
     }
 
+    /// <summary>
     /// Returns a new linked list containing only elements that are multiples of the given value.
+    /// </summary>
+    /// <param name="multiple">The divisor to filter elements.</param>
+    /// <returns>A new linked list containing only elements that are multiples of the given value.</returns>
     public ShortLinkedList GetMultiplesOf(short multiple)
     {
         ShortLinkedList result = new ShortLinkedList();
@@ -65,7 +99,9 @@ public class ShortLinkedList : IEnumerable<short>
         return result;
     }
 
-    /// Removes all elements greater than the average value.
+    /// <summary>
+    /// Removes all elements greater than the average value from the list.
+    /// </summary>
     public void RemoveElementsGreaterThanAverage()
     {
         List<short> values = this.ToList();
@@ -80,7 +116,10 @@ public class ShortLinkedList : IEnumerable<short>
         }
     }
 
-    /// Allows enumeration over the elements of the list.
+    /// <summary>
+    /// Returns an enumerator that iterates through the linked list.
+    /// </summary>
+    /// <returns>An enumerator for the list.</returns>
     public IEnumerator<short> GetEnumerator()
     {
         Node? current = head;
@@ -91,5 +130,9 @@ public class ShortLinkedList : IEnumerable<short>
         }
     }
 
+    /// <summary>
+    /// Returns a non-generic enumerator that iterates through the linked list.
+    /// </summary>
+    /// <returns>A non-generic enumerator for the list.</returns>
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
